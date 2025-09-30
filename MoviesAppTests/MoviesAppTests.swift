6597db1858899553bd0c1845b3f5e01d1fa79b2b
Kostas.Stergiannis <kostas.stergiannis98@gmail.com>
2025-09-30 14:52:40 +0300

    Initial Commit

---
+++ MoviesAppTests/MoviesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppTests.swift
+//  MoviesAppTests
+//
+//  Created by Konstantinos Stergiannis on 30/9/25.
+//
+
+import Testing
+@testable import MoviesApp
+
+struct MoviesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
